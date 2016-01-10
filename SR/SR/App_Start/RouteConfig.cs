a0aa3bb073d5e730d5ba26a680c81bc223fafd27
sam <am.sutcliffe2@gmail.com>
2016-01-10 00:32:26 +0000

    Update

--- SR/SR/App_Start/RouteConfig.cs
+++ SR/SR/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SR
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Game", action = "Lobby", id = UrlParameter.Optional }
             );
         }
     }
